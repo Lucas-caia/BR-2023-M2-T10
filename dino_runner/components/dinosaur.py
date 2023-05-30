e18646dd0e0f29e0375968fd2cc648bc0fe9be0a
Lucas-caia <caiaelucasft@gmail.com>
2023-05-30 17:49:10 -0300

    atualização

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,9 +1,22 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
+class AlternativeDinosaur:
+    def __init__(self):
+        # Inicialize os atributos do dinossauro alternativo aqui
+        pass
+
+    def update(self, user_input):
+        # Implemente a lógica de atualização do dinossauro alternativo aqui
+        pass
+
+    def draw(self, screen):
+        # Implemente a lógica de desenho do dinossauro alternativo aqui
+        pass
+
 
 class Dinosaur:
     def __init__(self):
@@ -15,6 +28,8 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
+
 
     def update(self, user_input):
         if self.dino_run:
@@ -25,10 +40,16 @@ class Dinosaur:
         if user_input [pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
         elif not self.dino_jump:
             self.dino_jump =False
             self.dino_run = True
-        
+            self.dino_duck = False
+
         if self.step_index >= 10:
             self.step_index = 0
 
@@ -52,7 +73,15 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        pass
+        if self.dino_duck:
+            if self.step_index >= 10:
+                self.step_index = 0
+            self.image = DUCKING[self.step_index // 5]  
+            self.dino_rect = self.image.get_rect()
+            self.dino_rect.x = X_POS
+            self.dino_rect.y = Y_POS + self.dino_rect.height // 2
+            self.step_index += 1  
 
-    def draw(self, screen):  
+    def draw(self, screen): 
+        self.duck() 
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -15,10 +15,12 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-
+        
         self.player = Dinosaur()
+        self.selected_dinosaur = None
 
     def run(self):
+        self.show_start_screen()
         # Game loop: events - update - draw
         self.playing = True
         while self.playing:
@@ -36,6 +38,28 @@ class Game:
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
 
+    def show_start_screen(self):
+        start_font = pygame.font.Font(None, 40)
+        start_text = start_font.render("Press any key to start", True, (0, 0, 0))
+        start_text_rect = start_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    running = False
+
+            self.screen.fill((255, 255, 255))
+            self.screen.blit(ICON, (SCREEN_WIDTH // 2 - ICON.get_width() // 2, SCREEN_HEIGHT // 2 - ICON.get_height() // 2 - 70))
+            self.screen.blit(start_text, start_text_rect)
+            pygame.display.update()
+            self.clock.tick(FPS)
+    
+
+
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
@@ -43,6 +67,7 @@ class Game:
         self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
+        
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -52,3 +77,19 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+  #  def dinosaur_selection_screen(self):
+  #      selected = False
+  #      while not selected:
+  #          for event in pygame.event.get():
+  #              if event.type == pygame.QUIT:
+  #                  pygame.quit()
+
+  #  def start_game(self):
+  #      self.dinosaur_selection_screen()
+  #      if self.selected_dinosaur == "default":
+  #          self.player = Dinosaur()
+   #     elif self.selected_dinosaur == "alternative":
+  #          self.player = AlternativeDinosaur()
+
+  #      self.run()
